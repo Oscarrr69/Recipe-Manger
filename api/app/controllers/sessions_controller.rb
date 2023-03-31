435089aeea55fc068baeea0b57d1a30d3e736de5
Khadaryussuf <salatkhadar@gmail.com>
2023-03-31 10:33:55 +0300

    Done

---
+++ api/app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    wrap_parameters format: []
+
+     # Login endpoint
+    def login
+        user = User.find_by(username: params[:username])
+        if user &.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, message: 'Login successfully', status: :ok
+        else
+            render json: { invalid: 'Invalid username or password' }, status: :unauthorized
+        end
+    end
+    
+    # Logout
+    def destroy
+        session.delete(:user_id)
+        render json: {message: 'Logged out'}, status: :ok
+    end
+        
+end
---
+++ api/app/serializers/favorite_serializer.rb
@@ -0,0 +1,5 @@
+class FavoriteSerializer < ActiveModel::Serializer
+  attributes :id, :origin, :comment, :user_id, :recipe_id
+  belongs_to :user 
+  belongs_to :recipe 
+end
